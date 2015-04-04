62cf41e5bc62799cfdd4681bdd8615cf7859fc2b
orbisnull <zen4dev@gmail.com>
2015-04-05 05:05:55 +0900

    move magic managers methods from repository to trait, add ability tu use DI->lazyGet in magic managers methods

---
+++ src/DeltaCore/Parts/MagicSetGetManagers.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * User: Vasiliy Shvakin (orbisnull) zen4dev@gmail.com
+ */
+
+namespace DeltaCore\Parts;
+
+
+trait MagicSetGetManagers
+{
+    /** @var \DeltaDb\Repository[]|Callable[] */
+    private $managers = [];
+
+    private function setManager($name, $object)
+    {
+        $this->managers[$name] = $object;
+    }
+
+    private function getManager($name)
+    {
+        if (!array_key_exists($name, $this->managers)) {
+            return null;
+        }
+        if (is_callable($this->managers[$name])) {
+            $this->managers[$name] = call_user_func($this->managers[$name]);
+        }
+        return $this->managers[$name];
+    }
+
+    function __call($name, $arguments)
+    {
+        $action = substr($name, 0, 3);
+        if (!$action) {
+            throw new \BadMethodCallException("Method " . $name . " not exist in " . get_class($this));
+        }
+        if (substr($name, -7, 7) === "Manager") {
+            $managerName = substr($name, 3, -7);
+            switch ($action) {
+                case "set":
+                    if (!isset($arguments[0]) || !is_object($arguments[0])) {
+                        throw new \InvalidArgumentException("try set empty object in {$managerName} manager");
+                    }
+                    $managerObject = $arguments[0];
+                    return $this->setManager($managerName, $managerObject);
+                    break;
+                case "get" :
+                    return $this->getManager($managerName);
+                    break;
+                default :
+                    throw new \BadMethodCallException("Managers may be only set and get, no method " . $name . " not exist in " . get_class($this));
+            }
+        }
+        throw new \BadMethodCallException("Method " . $name . " not exist in " . get_class($this));
+    }
+
+
+}
--- src/DeltaCore/Prototype/AbstractEntity.php
+++ src/DeltaCore/Prototype/AbstractEntity.php
@@ -6,12 +6,14 @@
 namespace DeltaCore\Prototype;
 
 
+use DeltaCore\Parts\MagicSetGetManagers;
+
 abstract class AbstractEntity
 {
+    use MagicSetGetManagers;
+
     protected $id;
     protected $fieldsList;
-    /** @var \DeltaDb\Repository[] */
-    private $managers = [];
 
     public function setId($id)
     {
@@ -44,41 +46,4 @@ abstract class AbstractEntity
         return $this->fieldsList;
     }
 
-    private function setManager($name, $object)
-    {
-        $this->managers[$name] = $object;
-    }
-
-    private function getManager($name)
-    {
-        return array_key_exists($name, $this->managers) ? $this->managers[$name] : null;
-    }
-
-    function __call($name, $arguments)
-    {
-        $action = substr($name, 0, 3);
-        if (!$action) {
-            throw new \BadMethodCallException("Method " . $name . " not exist in " . get_class($this));
-        }
-        if (substr($name, -7, 7) === "Manager") {
-            $managerName = substr($name, 3, -7);
-            switch ($action) {
-                case "set":
-                    if (!isset($arguments[0]) || !is_object($arguments[0])) {
-                        throw new \InvalidArgumentException("not found object for set manager");
-                    }
-                    $managerObject = $arguments[0];
-                    return $this->setManager($managerName, $managerObject);
-                    break;
-                case "get" :
-                    return $this->getManager($managerName);
-                    break;
-                default :
-                    throw new \BadMethodCallException("Managers may be only set and get, no method " . $name . " not exist in " . get_class($this));
-            }
-        }
-        throw new \BadMethodCallException("Method " . $name . " not exist in " . get_class($this));
-    }
-
-
-} 
+}

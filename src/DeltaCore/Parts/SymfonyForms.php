77c055dfb2201e530ce6a0777b4f1277bd92ba83
orbisnull <zen4dev@gmail.com>
2015-04-21 02:45:12 +0900

    change symfony forms trait

--- src/DeltaCore/Parts/SymfonyForms.php
+++ src/DeltaCore/Parts/SymfonyForms.php
@@ -12,11 +12,29 @@ trait SymfonyForms {
      */
     abstract public function getView();
 
-    /**
-     * @return \Symfony\Component\Form\FormBuilder
-     */
-    public function createFormBuilder()
+    public function createSfFormBuilder($name = null, array $options = array(), $data = null)
     {
-        return $this->getView()->getFormFactory()->createBuilder();
+        $type = 'form';
+        if ($name) {
+            return $this->getView()->getFormFactory()->createNamedBuilder($name, $type, $data, $options);
+        } else {
+            return $this->getView()->getFormFactory()->createBuilder($type, $data, $options);
+        }
+    }
+
+    public function entityToSfSelect($array, $id = "id", $name = "name")
+    {
+        $id = ucfirst($id);
+        $name = ucfirst($name);
+        $items = [];
+        foreach ($array as $item) {
+            $items[$item->{"get" . $id}()] = $item->{"get" . $name}();
+        }
+        uksort($items, function ($a, $b) {
+            $a = (integer)$a;
+            $b = (integer)$b;
+            return $a === $b ? 0 : $a < $b ? -1 : 1;
+        });
+        return $items;
     }
 }
